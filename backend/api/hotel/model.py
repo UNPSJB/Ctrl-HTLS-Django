bf6ceedc4021aecfc562f5573533d6c133292148
Montes Adriel <montes.adriel10@gmail.com>
2023-09-18 19:34:37 -0300

    #

--- backend/api/hotel/model.py
+++ backend/api/hotel/model.py
@@ -3,3 +3,6 @@ from django.db import models
 class Hotel(models.Model):
     nombre = models.CharField(max_length=100)
     direccion = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.nombre}, {self.direccion}"
