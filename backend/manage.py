bd8c0133187a92714780d18ceea69bc3879d3277
Montes Adriel <montes.adriel10@gmail.com>
2023-10-01 00:07:02 -0300

    Cargar todos los datos (loaddata_all)

--- backend/manage.py
+++ backend/manage.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -6,7 +5,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,8 +14,20 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+
+    # Cargar todos los datos de prueba si el comando es 'loaddata_all'
+    if sys.argv[1] == "loaddata_all":
+        # Se deben colocar en el orden segun quien necesita a quien
+        # Ejemplo hotel tiene direccion por lo tanto ubicacion(pais,provincia,ciudad,direccion) tiene que ir antes
+        fixture_dirs = ["ubicacion", "hotel", "persona"]
+        for dir in fixture_dirs:
+            print(f"Cargando datos desde {dir}/fixtures/data.json")
+            execute_from_command_line(
+                ["manage.py", "loaddata", f"{dir}/fixtures/data.json"]
+            )
+    else:
+        execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
