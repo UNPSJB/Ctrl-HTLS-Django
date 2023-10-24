2ece2319dbd447e35e621c0de45b2325abcbaa6d
Adriel-M-A <montes.adriel10@gmail.com>
2023-10-24 11:09:39 -0300

    Correccion de errores

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+// Crear una instancia de axios con la configuración predeterminada
 export const client = axios.create({
   baseURL: "http://localhost:8000/",
   headers: {
-    "X-Serializer": "full" // item, model, full
-  }
+    "X-Serializer": "full", // item, model, full
+  },
 });
--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,24 +1,42 @@
-import {client} from "./client";
+import { client } from "./client";
 
+// Definir los endpoints de la API
 const ENDPOINTS = [
-    ["hotel", "hotel", "hoteles"], 
-    ["hotel", "habitacion", "habitaciones"],
+  ["hotel", "hotel", "hoteles"],
+  ["hotel", "habitacion", "habitaciones"],
 ];
 
-const handleResponse = (resp) => resp.data;
-const handleError = (error) => {console.log(error); return "error";};
+// Manejar la respuesta de la API
+const handleResponse = (resp) => {
+  // Verificar si la respuesta es valida
+  if (resp && resp.data) return resp.data;
 
-const crud = ENDPOINTS.reduce((acc, endpoint) => {
-  const [app, singular, plural] = endpoint;
-  const titleSingular = singular[0].toUpperCase() + singular.substring(1); 
-  const titlePlural = plural[0].toUpperCase() + plural.substring(1); 
-  return Object.assign(acc, {
-    [`all${titlePlural}`]: () => client.get(`${app}/${plural}`).then(handleResponse).catch(handleError),
-    [`get${titleSingular}`]: (id) => client.get(`${app}/${plural}/${id}`).then(handleResponse).catch(handleError),
-    [`create${titleSingular}`]: (data) => client.post(`${app}/${plural}/${id}`, data).then(handleResponse).catch(handleError),
-    [`update${titleSingular}`]: (data) => client.put(`${app}/${plural}/${id}`, data).then(handleResponse).catch(handleError),
-    [`find${titlePlural}`]: (data) => client.get(`${app}/${plural}`, {params: data}).then(handleResponse).catch(handleError),
-  });
+  throw new Error("Respuesta de API no valida");
+};
+
+// Manejar los errores de la API
+const handleError = (error) => {
+  console.log(error);
+  return "error";
+};
+
+// Crear las funciones CRUD para cada endpoint
+const crud = ENDPOINTS.reduce((acc, [app, singular, plural]) => {
+  const titleSingular = `${singular[0].toUpperCase()}${singular.substring(1)}`;
+  const titlePlural = `${plural[0].toUpperCase()}${plural.substring(1)}`;
+  return {
+    ...acc,
+    [`all${titlePlural}`]: () =>
+      client.get(`${app}/${plural}`).then(handleResponse).catch(handleError),
+    [`get${titleSingular}`]: (id) =>
+      client.get(`${app}/${plural}/${id}`).then(handleResponse).catch(handleError),
+    [`create${titleSingular}`]: (data) =>
+      client.post(`${app}/${plural}`, data).then(handleResponse).catch(handleError),
+    [`update${titleSingular}`]: (id, data) =>
+      client.put(`${app}/${plural}/${id}`, data).then(handleResponse).catch(handleError),
+    [`find${titlePlural}`]: (data) =>
+      client.get(`${app}/${plural}`, { params: data }).then(handleResponse).catch(handleError),
+  };
 }, {});
 
 export default crud;
