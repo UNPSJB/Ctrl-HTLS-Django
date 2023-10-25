268cdde05be480ca243a8ceb57cab449764cff25
Montes Adriel <montes.adriel10@gmail.com>
2023-10-25 18:52:59 -0300

    Correccion HotelFull

--- backend/hotel/serializers.py
+++ backend/hotel/serializers.py
@@ -1,13 +1,24 @@
-from rest_framework.serializers import ModelSerializer
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import Hotel, Habitacion, HotelVendedor
-from core.models import Categoria, Vendedor
+from core.models import Categoria, Vendedor, Encargado, Direccion
+
+
+class DireccionSerializer(ModelSerializer):
+    class Meta:
+        model = Direccion
+        fields = ["calle", "numero"]
 
 
 class CategoriaSerializer(ModelSerializer):
     class Meta:
         model = Categoria
-        fields = ["id", "nombre"]
+        fields = ["nombre"]
+
+
+class EncargadoSerializer(ModelSerializer):
+    class Meta:
+        model = Encargado
+        fields = ["documento", "nombre", "apellido"]
 
 
 class VendedorSerializer(ModelSerializer):
@@ -24,6 +35,12 @@ class HotelVendedorSerializer(ModelSerializer):
         fields = ["vendedor"]
 
 
+class HabitacionSerializer(ModelSerializer):
+    class Meta:
+        model = Habitacion
+        fields = ["numero_de_habitacion", "piso", "hotel"]
+
+
 class HotelSerializer(ModelSerializer):
     class Meta:
         model = Hotel
@@ -31,19 +48,28 @@ class HotelSerializer(ModelSerializer):
 
 
 class HotelFullSerializer(ModelSerializer):
+    direccion = DireccionSerializer()
     categoria = CategoriaSerializer()
-    vendedores = serializers.SerializerMethodField()
+    encargado = EncargadoSerializer()
+    vendedores = SerializerMethodField()
+    habitaciones = SerializerMethodField()
 
     class Meta:
         model = Hotel
-        fields = ["id", "nombre", "direccion", "categoria", "encargado", "vendedores"]
+        fields = [
+            "id",
+            "nombre",
+            "direccion",
+            "categoria",
+            "encargado",
+            "vendedores",
+            "habitaciones",
+        ]
 
     def get_vendedores(self, obj):
         vendedores = HotelVendedor.objects.filter(hotel=obj)
         return HotelVendedorSerializer(vendedores, many=True).data
 
-
-class HabitacionSerializer(ModelSerializer):
-    class Meta:
-        model = Habitacion
-        fields = ["numero_de_habitacion", "piso", "precio", "hotel"]
+    def get_habitaciones(self, obj):
+        habitaciones = Habitacion.objects.filter(hotel=obj)
+        return HabitacionSerializer(habitaciones, many=True).data
