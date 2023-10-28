7352252add0d6d6c2f30d02279a4fc53aa341ba3
Montes Adriel <montes.adriel10@gmail.com>
2023-10-28 19:55:03 -0300

    Correccion errores

--- backend/venta/models.py
+++ backend/venta/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from hotel.models import Hotel
 
+
 class Alquiler(models.Model):
     fecha_inicio = models.DateField()
     fecha_fin = models.DateField()
     pasajeros = models.IntegerField()
-    importe = coeficiente_descuento = models.DecimalField(max_digits=10, decimal_places=2)
-    
+    # importe = coeficiente_descuento = models.DecimalField(max_digits=10, decimal_places=2)
+
     def __str__(self):
         return self.importe
-
