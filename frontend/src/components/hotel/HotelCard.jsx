9f8cadf4f092c3a318650752a45c0503727b8bc5
Montes Adriel <montes.adriel10@gmail.com>
2023-10-29 00:37:13 -0300

    #

--- frontend/src/components/hotel/HotelCard.jsx
+++ frontend/src/components/hotel/HotelCard.jsx
@@ -1,13 +1,35 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../../api";
 
-function HotelCard({ hoteles }) {
+function HotelCard({ hoteles, setHoteles }) {
+  const navigate = useNavigate();
+  const handleDelete = async (id) => {
+    try {
+      await api.hoteles.delete(id);
+      console.log(`Hotel con id ${id} eliminado`);
+      const updatedHoteles = hoteles.filter((hotel) => hotel.id !== id);
+      setHoteles(updatedHoteles); // Actualiza los hoteles después de la eliminación
+      navigate("/hoteles");
+    } catch (error) {
+      console.error(`Error al eliminar el hotel con id ${id}: `, error);
+    }
+  };
+
   return (
     <div>
       {hoteles.map((hotel) => (
-        <div key={hotel.id}>
+        <div className="flex justify-between space-x-20 " key={hotel.id}>
           <h1>
             <Link to={`/hotel/${hotel.id}`}>{hotel.nombre}</Link>
           </h1>
+          <div className="space-x-10">
+            <button className="text-sky-500" onClick={() => console.log("Modificar")}>
+              Modificar
+            </button>
+            <button className="text-red-500" onClick={() => handleDelete(hotel.id)}>
+              Eliminar
+            </button>
+          </div>
         </div>
       ))}
     </div>
--- frontend/src/components/hotel/HotelList.jsx
+++ frontend/src/components/hotel/HotelList.jsx
@@ -6,12 +6,11 @@ import HotelCard from "./HotelCard";
 export default function HotelList({ pais, provincia, ciudad, categoria }) {
   const [hoteles, setHoteles] = useState([]);
 
-  // Listado de Hoteles según el país, provincia o ciudad seleccionada
   useEffect(() => {
     api.hoteles.find({ pais, provincia, ciudad, categoria }).then((res) => {
       setHoteles(res);
     });
   }, [pais, provincia, ciudad, categoria]);
 
-  return <HotelCard hoteles={hoteles} />;
+  return <HotelCard hoteles={hoteles} setHoteles={setHoteles} />;
 }
