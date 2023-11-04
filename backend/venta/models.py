0203818846bb9bc85ad977c0c223725d18e19fa2
Adriel-M-A <montes.adriel10@gmail.com>
2023-11-03 22:24:48 -0300

    #

--- backend/venta/models.py
+++ backend/venta/models.py
@@ -14,7 +14,6 @@ class Alquiler(models.Model):
 
     def __str__(self):
         return self.importe
-    
 
 
 class Factura(models.Model):
@@ -22,12 +21,11 @@ class Factura(models.Model):
     numero = models.IntegerField()
     tipo_factura = models.CharField(max_length=1)
     importe_total = models.DecimalField(max_digits=10, decimal_places=2)
-    
-    
+
     def __str__(self):
         return f"Factura {self.numero} con total de: {self.importe_total}"
-    
-    
+
+
 class Detalle_factura(models.Model):
     descripcion = models.CharField(max_length=80)
     importe = models.DecimalField(max_digits=10, decimal_places=2)
@@ -40,11 +38,13 @@ class Liquidacion(models.Model):
     fecha_emision = models.DateField()
     fecha_pago = models.DateField(null=True)
     monto = models.DecimalField(max_digits=10, decimal_places=2)
-    detalle_factura = models.ForeignKey(Detalle_factura, on_delete=models.CASCADE, null=True)
-    
+    detalle_factura = models.ForeignKey(
+        Detalle_factura, on_delete=models.CASCADE, null=True
+    )
+
     def __str__(self):
         return f"Liquidacion {self.numero} con fecha {self.fecha_emision}"
-    
+
 
 class Pago(models.Model):
     CONTADO = 0
@@ -54,7 +54,6 @@ class Pago(models.Model):
     fecha = models.DateField()
     TIPOS_PAGO = ((CONTADO, "CONTADO"), (PUNTOS, "PUNTOS"), (TARJETA, "TARJETA"))
     tipo_pago = models.PositiveSmallIntegerField(choices=TIPOS_PAGO)
-    
+
     def __str__(self):
         return f"Pago {self.importe} abonado con {self.tipo_pago}"
-
