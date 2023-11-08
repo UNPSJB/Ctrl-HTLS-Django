bd35eee14b3a7a3a64872a1a6107ec4a7bc43ec7
Adriel-M-A <montes.adriel10@gmail.com>
2023-11-07 21:58:51 -0300

    Testeo

---
+++ frontend/src/components/hotel/Card.jsx
@@ -0,0 +1,32 @@
+function HotelCard({ hotel }) {
+  return (
+    <div className="p-4 m-2 border rounded shadow-lg flex justify-between">
+      <div className="w-3/4">
+        <h2 className="text-xl font-bold">{hotel.nombre}</h2>
+        <p className="text-sm text-gray-500">
+          {hotel.categoria.estrellas} estrellas
+        </p>
+        <p>
+          {hotel.ubicacion.ciudad} - {hotel.ubicacion.provincia} -{" "}
+          {hotel.ubicacion.pais}
+        </p>
+        <p className="mt-2 text-gray-700">{hotel.descripcion}</p>
+      </div>
+      <div className="w-1/4 flex flex-col items-end">
+        <button className="px-4 py-2 mb-2 text-white bg-green-500 rounded">
+          Toggle
+        </button>
+        <div className="mt-auto">
+          <button className="px-4 py-2 mr-2 text-white bg-blue-500 rounded">
+            Modificar
+          </button>
+          <button className="px-4 py-2 text-white bg-red-500 rounded">
+            Eliminar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HotelCard;
