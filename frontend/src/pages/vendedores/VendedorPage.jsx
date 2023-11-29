7f24351eb31906a7243bad89b00954cf74a8adbd
Pablo <pavezpabloandres@gmail.com>
2023-11-29 09:56:22 -0300

    Se agregó estilo a la vendedorPage

--- frontend/src/pages/vendedores/VendedorPage.jsx
+++ frontend/src/pages/vendedores/VendedorPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from "../../api";
+import Header from "../../components/header/Header";
 
 export default function VendedorPage() {
   const { documento } = useParams();
@@ -12,18 +13,28 @@ export default function VendedorPage() {
     });
   }, [documento]);
 
+  const secondNavBarChildren = (
+    <>
+      <h2 className="text-3xl">{vendedor?.nombre} {vendedor?.apellido}</h2>
+    </>
+  );
+
   return (
     <div>
-      {vendedor ? (
-        <>
-          <p>Documento: {vendedor.documento}</p>
-          <p>Tipo de Documento: {vendedor.tipo_documento}</p>
-          <p>Nombre: {vendedor.nombre}</p>
-          <p>Apellido: {vendedor.apellido}</p>
-        </>
+      <Header secondNavBarChildren={secondNavBarChildren} />
+      <div className="flex justify-center">
+      {vendedor ? (        
+      <div className="p-6 bg-white rounded shadow-md w-80 text-center">
+        <h2 className="text-2xl font-bold mb-2">Información del Vendedor</h2>
+        <p><span className="font-bold">Documento:</span> {vendedor.documento}</p>
+        <p><span className="font-bold">Tipo de Documento:</span> {vendedor.tipo_documento}</p>
+        <p><span className="font-bold">Nombre:</span> {vendedor.nombre}</p>
+        <p><span className="font-bold">Apellido:</span> {vendedor.apellido}</p>
+      </div>
       ) : (
         <p>Cargando...</p>
       )}
+      </div>
     </div>
   );
 }
