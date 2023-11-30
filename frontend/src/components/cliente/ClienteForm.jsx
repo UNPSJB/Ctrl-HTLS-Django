d883ddc474cc3d61731aa248413fcbabc4dcfa82
Pablo <pavezpabloandres@gmail.com>
2023-11-30 08:04:03 -0300

    Se agregó estilo al ClienteForm

--- frontend/src/components/cliente/ClienteForm.jsx
+++ frontend/src/components/cliente/ClienteForm.jsx
@@ -6,12 +6,14 @@ import api from "../../api";
 import { useState } from "react";
 import SelectTipoDocumento from "../selectores/SelectTipoDocumento";
 import { useForm } from "react-hook-form";
+import SuccessModal from '../../components/successModal'; 
 
 export default function ClienteForm({ title, isOpen, onClose }) {
   const [pais, setPais] = useState("");
   const [provincia, setProvincia] = useState("");
   const [ciudad, setCiudad] = useState("");
   const [tipoDocumento, setTipoDocumento] = useState("");
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
 
   const {
     register,
@@ -39,6 +41,9 @@ export default function ClienteForm({ title, isOpen, onClose }) {
     } catch (error) {
       console.error("ERROR", error);
     }
+
+    setShowSuccessModal(true);
+
   });
 
   return (
@@ -48,6 +53,7 @@ export default function ClienteForm({ title, isOpen, onClose }) {
           type="text"
           placeholder="nombre"
           {...register("nombre", { required: true })}
+          className="w-full h-9 p-2 mb-2 bg-white rounded-lg border-2 border-violet-500 flex-col justify-start items-start gap-2.5 inline-flex"
         />
         {errors.nombre && <span>Este campo es requerido</span>}
 
@@ -55,53 +61,65 @@ export default function ClienteForm({ title, isOpen, onClose }) {
           type="text"
           placeholder="apellido"
           {...register("apellido", { required: true })}
+          className="w-full h-9 p-2 mb-2 bg-white rounded-lg border-2 border-violet-500 flex-col justify-start items-start gap-2.5 inline-flex"
         />
         {errors.apellido && <span>Este campo es requerido</span>}
         <SelectTipoDocumento
           tipoDocumento={tipoDocumento}
           setTipoDocumento={setTipoDocumento}
+          className="w-full h-32 p-2 mb-2 bg-white-100 rounded-lg border-2 border-blue-500 flex-col justify-start items-start gap-2.5 inline-flex"
         />
         <input
           type="number"
           placeholder="documento"
           {...register("documento", { required: true })}
+          className="w-full h-9 p-2 mb-2 bg-white rounded-lg border-2 border-violet-500 flex-col justify-start items-start gap-2.5 inline-flex"
         />
         {errors.documento && <span>Este campo es requerido</span>}
-        <SelectPais pais={pais} setPais={setPais} />
+        <SelectPais pais={pais} setPais={setPais}
+        className="w-full h-10 p-2 mb-2 bg-white rounded-lg border-2 border-violet-500 flex-col justify-start items-start gap-2.5 inline-flex"
+         />
         <SelectProvincia
           pais={pais}
           provincia={provincia}
           setProvincia={setProvincia}
+          className="w-full h-10 p-2 mb-2 bg-white rounded-lg border-2 border-violet-500 flex-col justify-start items-start gap-2.5 inline-flex"
         />
         <SelectCiudad
           provincia={provincia}
           ciudad={ciudad}
           setCiudad={setCiudad}
+          className="w-full h-10 p-2 mb-2 bg-white rounded-lg border-2 border-violet-500 flex-col justify-start items-start gap-2.5 inline-flex"
         />
         <input
           type="text"
           placeholder="calle"
           {...register("calle", { required: true })}
+          className="w-full h-9 p-2 mb-2 bg-white rounded-lg border-2 border-violet-500 flex-col justify-start items-start gap-2.5 inline-flex"
         />
         {errors.calle && <span>Este campo es requerido</span>}
         <input
           type="number"
           placeholder="numero"
           {...register("numero", { required: true })}
+          className="w-full h-9 p-2 mb-2 bg-white rounded-lg border-2 border-violet-500 flex-col justify-start items-start gap-2.5 inline-flex"
         />
         {errors.numero && <span>Este campo es requerido</span>}
         <input
           type="number"
           placeholder="telefono"
           {...register("telefono", { required: true })}
+          className="w-full h-9 p-2 mb-2 bg-white rounded-lg border-2 border-violet-500 flex-col justify-start items-start gap-2.5 inline-flex"
         />
         {errors.telefono && <span>Este campo es requerido</span>}
         <input
           type="correo"
           placeholder="e-mail"
           {...register("correo", { required: true })}
+          className="w-full h-9 p-2 mb-2 bg-white rounded-lg border-2 border-violet-500 flex-col justify-start items-start gap-2.5 inline-flex"
         />
         {errors.email && <span>Este campo es requerido</span>}
+        
 
         {/* Botones en la misma fila */}
         <div className="flex justify-end mt-4">
@@ -119,6 +137,8 @@ export default function ClienteForm({ title, isOpen, onClose }) {
           </button>
         </div>
       </form>
+      {/* Mostrar el modal de éxito */}
+      <SuccessModal show={showSuccessModal} onClose={() => setShowSuccessModal(false)} />
     </Modal>
   );
 }
