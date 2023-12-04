1385c4bb86b845124904321a000d6ed1a7469442
Pablo <pavezpabloandres@gmail.com>
2023-12-04 07:45:47 -0300

    #

--- frontend/src/pages/vendedores/VendedorPage.jsx
+++ frontend/src/pages/vendedores/VendedorPage.jsx
@@ -35,7 +35,7 @@ export default function VendedorPage() {
       <div className="p-6 bg-white rounded shadow-md w-80 text-center">
         <h2 className="text-2xl font-bold mb-2">Información del Vendedor</h2>
         <p><span className="font-bold">Documento:</span> {vendedor.documento}</p>
-        <p><span className="font-bold">Tipo de Documento:</span> {vendedor.tipo_documento}</p>
+        {/* <p><span className="font-bold">Tipo de Documento:</span> {vendedor.tipo_documento}</p>*/}
         <p><span className="font-bold">Nombre:</span> {vendedor.nombre}</p>
         <p><span className="font-bold">Apellido:</span> {vendedor.apellido}</p>
       </div>
