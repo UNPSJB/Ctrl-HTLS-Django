96b51af2db3f3eed24e03796d6b6cc65125452c6
Adriel-M-A <montes.adriel10@gmail.com>
2024-01-17 20:38:06 -0300

    Reinicio

--- backend/venta/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import AlquilerViewSet
-
-router = DefaultRouter()
-router.register(r"alquileres", AlquilerViewSet, basename="alquiler")
-
-urlpatterns = router.urls
